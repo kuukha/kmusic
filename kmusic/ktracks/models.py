b0bc085fcbcf7eb4117c1b8aabca99297e0f7a65
Robert <robertwalulya@gmail.com>
2020-08-15 18:13:11 +0300

    backend

--- kmusic/ktracks/models.py
+++ kmusic/ktracks/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from datetime import datetime
 
 # Create your models here.
 
 class Ktrack(models.Model):
 
-    tittle = models.CharField(max_length=50)
-    description = models.TextField(blank=True)
-    url = models.URLField()
-    cerated_at = models.DateTimeField(auto_now_add=True)
+    tittle = models.CharField(max_length=50, null=True)
+    description = models.TextField(blank=True, null=True)
+    url = models.URLField(null=True)
+    cerated_at = models.DateTimeField(default=datetime.now)
+    posted_by = models.ForeignKey(get_user_model(), null=True, on_delete=models.CASCADE)
+
+class Like(models.Model):
+    user = models.ForeignKey(get_user_model(), null=True, on_delete=models.CASCADE) 
+    ktrack = models.ForeignKey('ktracks.Ktrack', related_name='likes', on_delete=models.CASCADE)
--- kmusic/users/schema.py
+++ kmusic/users/schema.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 import graphene
-
+from graphql import GraphQLError
 from graphene_django import DjangoObjectType
 
 class UserType(DjangoObjectType):
@@ -18,7 +18,7 @@ class Query(graphene.ObjectType):
     def resolve_me(self, info):
         user = info.context.user
         if user.is_anonymous:
-            raise Exception('Not logged in')
+            raise GraphQLError('Not logged in')
         return user
 
 
